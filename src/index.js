8272949a69924edde66f1959d68a243a5094fbc2
Vladimir Kovalchuk <Vladimir6332@gmail.com>
2019-03-14 23:44:11 +0300

    feat: add func for count zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let arrParametrs = {};
+    let counterZeros = number;
+
+    for (let i = 2; i <= base; i++) {
+        if (base % i == 0) {
+            base = base / i;
+
+            if (arrParametrs[i]) arrParametrs[i]++
+                else arrParametrs[i] = 1;
+            i = 1;
+        }
+    }
+
+    for (let key in arrParametrs) {
+        let currentCounter = 0;
+        let k;
+        for (let j = 1;
+            (k = Math.pow(key, j)) <= number; j++) {
+            currentCounter += (number - (number % k)) / k;
+        }
+        currentCounter = (currentCounter - (currentCounter % arrParametrs[key])) / arrParametrs[key];
+        if (currentCounter < counterZeros) counterZeros = currentCounter;
+    }
+    return counterZeros;
 }
